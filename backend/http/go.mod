020d55db972030d2e477c87ac75abaa775b490d1
Pierre Carrier <pc@rrier.fr>
2023-10-12 22:46:22 +0200

    frontend

---
+++ backend/http/go.mod
@@ -0,0 +1,10 @@
+module github.com/pcarrier/ident.me/backend/http
+
+go 1.17
+
+require github.com/redis/go-redis/v9 v9.2.1
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
---
+++ backend/http/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"net/http"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{})
+	if err := http.ListenAndServe("localhost:8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		i, err := client.Incr(r.Context(), "counter").Result()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		} else {
+			_, _ = w.Write([]byte(fmt.Sprintf("%08x", i)))
+		}
+	})); err != nil {
+		panic(err)
+	}
+}
