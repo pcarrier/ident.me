7d42b1ec17fc722f2d707b789f9c31d32b2a732d
Pierre Carrier <pc@rrier.fr>
2025-11-04 03:38:49 +0000

    nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    xmit = {
+      url = "github:xmit-co/xmit";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      xmit,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nixfmt
+              go
+              pnpm
+              xmit.packages.${system}.default
+            ];
+          };
+        };
+      }
+    );
+}
